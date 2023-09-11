1f37b8361697e2aa0eb410d1f63b8137b99d12ef
milanrolie <112857190+milanrolie@users.noreply.github.com>
2023-09-11 14:48:26 +0200

    lenis toegevoegd

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,46 @@
+<script>
+    import { onMount } from "svelte";
+    import Lenis from "@studio-freight/lenis";
+  
+    onMount(() => {
+      const lenis = new Lenis({
+        duration: 1.5,
+        easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
+        orientation: "vertical",
+        gestureOrientation: "vertical",
+        smoothWheel: true,
+        smoothTouch: false,
+        touchMultiplier: 1.1,
+      });
+  
+      function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+      }
+  
+      requestAnimationFrame(raf);
+  
+      lenis.on("scroll", () => {});
+  
+      gsap.ticker.add((time) => {
+        lenis.raf(time * 1000);
+      });
+  
+      gsap.ticker.lagSmoothing(0);
+    });
+  </script>
+
+
+
+
+
+
+<main>
+<slot />
+</main>
+
+<style>
+
+
+ 
+</style>
